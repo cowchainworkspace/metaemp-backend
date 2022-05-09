63708bfb434bf844b9ce25b7116e3ccd5ed9e7ca
tweekerwork <vladyslav.fit@cowchain.io>
2022-05-09 13:25:24 +0300

    Companies Filter for Admin

--- src/MetaEmp.Application/Features/Protected/Companies/All/FilterCompaniesRequest.cs
+++ src/MetaEmp.Application/Features/Protected/Companies/All/FilterCompaniesRequest.cs
@@ -7,5 +7,6 @@ namespace MetaEmp.Application.Features.Protected.Companies.All;
 
 public class FilterCompaniesRequest : IRequest<CompanyResult[]>
 {
-    public ApprovingStatus Status { get; set; }
+    public string? Name { get; set; }
+    public ApprovingStatus? Status { get; set; }
 }
