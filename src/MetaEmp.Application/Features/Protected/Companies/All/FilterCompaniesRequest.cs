245b4ea901c40b85888cbc412130fbc4f3d788ea
tweekerwork <vladyslav.fit@cowchain.io>
2022-05-09 14:05:10 +0300

    Companies sorting added

--- src/MetaEmp.Application/Features/Protected/Companies/All/FilterCompaniesRequest.cs
+++ src/MetaEmp.Application/Features/Protected/Companies/All/FilterCompaniesRequest.cs
@@ -7,5 +7,7 @@ namespace MetaEmp.Application.Features.Protected.Companies.All;
 
 public class FilterCompaniesRequest : IRequest<CompanyResult[]>
 {
-    public ApprovingStatus Status { get; set; }
+    public string? Name { get; set; }
+    public string? SortFilter { get; set; }
+    public ApprovingStatus? Status { get; set; }
 }
